64e06bbbdc16d38661405ca9f1fae76ca9dabc3a
zazou2004 <isabellamiralles12@gmail.com>
2025-11-20 12:17:28 +0100

    debut initi poids

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import numpy as np
 class Reseau_Neurones:
-    def __init__(self, nombre_couche, nombre_neurone_couche, valeur_init_poids, fonction_activation, nb_sortie):
+    def __init__(self,taille_entree, nombre_couche, nombre_neurone_couche, valeur_init_poids, fonction_activation, nb_sortie):
         #definition des neurones avec poids
         #nombre_neurone_couche = liste de nb de couche elt
+        self.nb_neurone_couche = [taille_entree] + nombre_neurone_couche + nb_sortie # la première couche dépend de la taille de l'image, la dernière ici 1 ou 0 donc 1
         self.nb_couche= nombre_couche
-        self.nb_neurone_couche= nombre_neurone_couche
         self.f_activation=fonction_activation
         self.nb_sorties= nb_sortie
+        self.poids=[]
+        self.biais=[]
+
+        self.init_poids()
 
     def init_poids(self):
         for i in range(self.nb_couche):
-            self.poids=np.random(0,1)
+            taille_entree_couche = self.tailles_couches[i]
+            taille_sortie_couche = self.tailles_couches[i + 1]
+            W= np.random.uniform(low=0.0, high=1.0, size=(taille_entree_couche,taille_sortie_couche))
+            self.poids.append(W)
+
 
     def ouvrir_image(self,image):
         #divise l'image en pixel liste numpy de nuances de gris entre 0 et 255
